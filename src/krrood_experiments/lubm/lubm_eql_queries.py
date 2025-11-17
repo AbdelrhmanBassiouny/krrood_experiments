3f715a1b18b148f902ca04f4ffbe08ddc79f5f31
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-11-17 18:17:38 +0100

    [LUBM] Corrected jinja_template.j2

--- src/krrood_experiments/lubm/lubm_eql_queries.py
+++ src/krrood_experiments/lubm/lubm_eql_queries.py
@@ -3,7 +3,6 @@ import time
 from typing import List
 
 from krrood.entity_query_language.entity import (
-    symbolic_mode,
     a,
     flatten,
     contains,
@@ -13,12 +12,12 @@ from krrood.entity_query_language.entity import (
 from krrood.entity_query_language.predicate import HasType
 from krrood.entity_query_language.symbol_graph import SymbolGraph
 from krrood.entity_query_language.symbolic import ResultQuantifier
-from krrood.experiments import lubm_with_predicates
-from krrood.experiments.helpers import (
+from krrood_experiments.lubm import lubm_with_predicates
+from krrood_experiments.lubm.helpers import (
     evaluate_eql,
     load_instances_for_lubm_with_predicates,
 )
-from krrood.experiments.lubm_with_predicates import (
+from krrood_experiments.lubm.lubm_with_predicates import (
     GraduateStudent,
     Person,
     Publication,
@@ -39,141 +38,127 @@ def get_eql_queries() -> List[ResultQuantifier]:
     # 1 (No joining, just filtration of graduate students through taking a certain course)
     SymbolGraph().clear()
     SymbolGraph.build(classes=classes_of_module(lubm_with_predicates))
-    with symbolic_mode():
-        q1 = a(
-            x := GraduateStudent(),
-            flatten(x.takes_course).uri
-            == "http://www.Department0.University0.edu/GraduateCourse0",
-        )
+    q1 = a(
+        x := GraduateStudent(),
+        flatten(x.takes_course).uri
+        == "http://www.Department0.University0.edu/GraduateCourse0",
+    )
 
     # 2
-    with symbolic_mode():
-        q2 = a(
-            x := GraduateStudent(),
-            HasType(
-                z := flatten(x.person.member_of), Department
-            ),  # filtration of x producing z
-            HasType(
-                y := flatten(z.sub_organization_of), University
-            ),  # filtration of z (which in turn filters x again) producing y
-            contains(x.person.undergraduate_degree_from, y),  # join between x and y
-        )
+    q2 = a(
+        x := GraduateStudent(),
+        HasType(
+            z := flatten(x.person.member_of), Department
+        ),  # filtration of x producing z
+        HasType(
+            y := flatten(z.sub_organization_of), University
+        ),  # filtration of z (which in turn filters x again) producing y
+        contains(x.person.undergraduate_degree_from, y),  # join between x and y
+    )
 
     # 3
-    with symbolic_mode():
-        q3 = a(
-            x := Publication(),
-            flatten(x.publication_author).uri
-            == "http://www.Department0.University0.edu/AssistantProfessor0",
-        )
+    q3 = a(
+        x := Publication(),
+        flatten(x.publication_author).uri
+        == "http://www.Department0.University0.edu/AssistantProfessor0",
+    )
 
     # 4
-    with symbolic_mode():
-        q4 = a(
-            set_of(
-                (
-                    x := Professor(),
-                    name := x.name,
-                    email := x.person.email_address,
-                    telephone := x.person.telephone,
-                ),
-                flatten(x.works_for).uri == "http://www.Department0.University0.edu",
-            )
+    q4 = a(
+        set_of(
+            (
+                x := Professor(),
+                name := x.name,
+                email := x.person.email_address,
+                telephone := x.person.telephone,
+            ),
+            flatten(x.works_for).uri == "http://www.Department0.University0.edu",
         )
+    )
 
     # 5
-    with symbolic_mode():
-        q5 = a(
-            x := Person(),
-            flatten(x.member_of).uri == "http://www.Department0.University0.edu",
-        )
+    q5 = a(
+        x := Person(),
+        flatten(x.member_of).uri == "http://www.Department0.University0.edu",
+    )
 
     # 6
-    with symbolic_mode():
-        q6 = a(x := Student())
+    q6 = a(x := Student())
 
     # 7
-    with symbolic_mode():
-        associate_professor = the(
-            AssociateProfessor(
-                uri="http://www.Department0.University0.edu/AssociateProfessor0"
-            )
+    associate_professor = the(
+        AssociateProfessor(
+            uri="http://www.Department0.University0.edu/AssociateProfessor0"
         )
-        q7 = a(
-            set_of(
-                (
-                    x := Student(),
-                    y := flatten(x.takes_course),
-                ),
-                contains(associate_professor.teacher_of, y),
-            )
+    )
+    q7 = a(
+        set_of(
+            (
+                x := Student(),
+                y := flatten(x.takes_course),
+            ),
+            contains(associate_professor.teacher_of, y),
         )
+    )
 
     # 8
-    with symbolic_mode():
-        q8 = a(
-            set_of(
-                (
-                    x := Student(),
-                    y := flatten(x.person.member_of),
-                    z := x.person.email_address,
-                ),
-                HasType(y, Department),
-                flatten(y.sub_organization_of).uri == "http://www.University0.edu",
-            )
+    q8 = a(
+        set_of(
+            (
+                x := Student(),
+                y := flatten(x.person.member_of),
+                z := x.person.email_address,
+            ),
+            HasType(y, Department),
+            flatten(y.sub_organization_of).uri == "http://www.University0.edu",
         )
+    )
 
     # 9
-    with symbolic_mode():
-        q9 = a(
-            set_of(
-                (
-                    x := Student(),
-                    y := flatten(x.person.advisor),
-                    z := flatten(x.takes_course),
-                ),
-                HasType(y, Faculty),
-                contains(
-                    y.teacher_of, z
-                ),  # will benefit from symbol graph optimization
-            )
+    q9 = a(
+        set_of(
+            (
+                x := Student(),
+                y := flatten(x.person.advisor),
+                z := flatten(x.takes_course),
+            ),
+            HasType(y, Faculty),
+            contains(
+                y.teacher_of, z
+            ),  # will benefit from symbol graph optimization
         )
+    )
 
     # 10
-    with symbolic_mode():
-        q10 = a(
-            x := Student(),
-            flatten(x.takes_course).uri
-            == "http://www.Department0.University0.edu/GraduateCourse0",
-        )
+    q10 = a(
+        x := Student(),
+        flatten(x.takes_course).uri
+        == "http://www.Department0.University0.edu/GraduateCourse0",
+    )
 
     # 11
-    with symbolic_mode():
-        q11 = a(
-            x := ResearchGroup(),
-            flatten(x.sub_organization_of).uri == "http://www.University0.edu",
-        )
+    q11 = a(
+        x := ResearchGroup(),
+        flatten(x.sub_organization_of).uri == "http://www.University0.edu",
+    )
 
     # 12
-    with symbolic_mode():
-        q12 = a(
-            set_of(
-                (x := Chair(), y := flatten(x.works_for)),
-                HasType(y, Department),
-                flatten(y.sub_organization_of).uri == "http://www.University0.edu",
-            )  # writing contains like this implies that the user knows that this is a set of objects.
-            # A more declarative way would be to write SubOrganizationOf(y, the(University(name="University0"))).
-        )
+    q12 = a(
+        set_of(
+            (x := Chair(), y := flatten(x.works_for)),
+            HasType(y, Department),
+            flatten(y.sub_organization_of).uri == "http://www.University0.edu",
+        )  # writing contains like this implies that the user knows that this is a set of objects.
+        # A more declarative way would be to write SubOrganizationOf(y, the(University(name="University0"))).
+    )
 
     # 13
-    with symbolic_mode():
-        q13 = a(
-            x := flatten(the(University(uri="http://www.University0.edu")).has_alumnus),
-        )
+    q13 = a(
+        x := flatten(the(University(uri="http://www.University0.edu")).has_alumnus),
+    )
 
     # 14
-    with symbolic_mode():
-        q14 = a(x := UndergraduateStudent())
+    q14 = a(x := UndergraduateStudent())
 
     eql_queries = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14]
     return eql_queries
