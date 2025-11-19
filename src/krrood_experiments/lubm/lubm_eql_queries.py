ece08418fb0dcfbe69e63f3f3a936908b13958d2
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2025-11-19 17:08:51 +0100

    [LUBM] updating queries to use match.

--- src/krrood_experiments/lubm/lubm_eql_queries.py
+++ src/krrood_experiments/lubm/lubm_eql_queries.py
@@ -7,12 +7,16 @@ from krrood.entity_query_language.entity import (
     flatten,
     contains,
     set_of,
-    the, let, entity, an, exists, and_
+    the,
+    let,
+    entity,
+    an,
+    exists,
+    and_,
+    match,
 )
 from krrood.entity_query_language.predicate import HasType
-from krrood.entity_query_language.symbol_graph import SymbolGraph
 from krrood.entity_query_language.symbolic import ResultQuantifier
-
 from krrood_experiments.lubm.helpers import (
     evaluate_eql,
     load_instances_for_lubm_with_predicates,
@@ -30,35 +34,50 @@ from krrood_experiments.lubm.lubm_with_predicates import (
     ResearchGroup,
     Chair,
     UndergraduateStudent,
+    Course,
 )
 
 
 def get_eql_queries() -> List[ResultQuantifier]:
     # 1 (No joining, just filtration of graduate students through taking a certain course)
-    q1 = an(entity(
-        x := let(GraduateStudent, domain=None),
-        flatten(x.takes_course).uri
-        == "http://www.Department0.University0.edu/GraduateCourse0"
-    ))
+    q1 = a(
+        match(GraduateStudent)(
+            takes_course=match(Course)(
+                uri="http://www.Department0.University0.edu/GraduateCourse0"
+            )
+        )
+    )
 
     # 2
-    q2 = an(entity(
-        x := let(GraduateStudent, domain=None),
-        HasType(
-            z := flatten(x.person.member_of), Department
-        ),  # filtration of x producing z
-        HasType(
-            y := flatten(z.sub_organization_of), University
-        ),  # filtration of z (which in turn filters x again) producing y
-        contains(x.person.undergraduate_degree_from, y),  # join between x and y
-    ))
+    q2 = a(
+        match(GraduateStudent)(
+            person=match(Person)(
+                member_of=match(Department)(
+                    sub_organization_of=(uni := match(University)())
+                ),
+                undergraduate_degree_from=uni,
+            )
+        )
+    )
+
+    # 2
+    q2 = an(
+        entity(
+            x := let(GraduateStudent, domain=None),
+            HasType(z := flatten(x.person.member_of), Department),
+            HasType(y := flatten(z.sub_organization_of), University),
+            contains(x.person.undergraduate_degree_from, y),
+        )
+    )
 
     # 3
-    q3 = an(entity(
-        x := let(Publication, domain=None),
-        flatten(x.publication_author).uri
-        == "http://www.Department0.University0.edu/AssistantProfessor0",
-    ))
+    q3 = an(
+        entity(
+            x := let(Publication, domain=None),
+            flatten(x.publication_author).uri
+            == "http://www.Department0.University0.edu/AssistantProfessor0",
+        )
+    )
 
     # 4
     q4 = a(
@@ -74,19 +93,24 @@ def get_eql_queries() -> List[ResultQuantifier]:
     )
 
     # 5
-    q5 = an(entity(
-        x := let(Person, domain=None),
-        flatten(x.member_of).uri == "http://www.Department0.University0.edu",
-    ))
+    q5 = an(
+        entity(
+            x := let(Person, domain=None),
+            flatten(x.member_of).uri == "http://www.Department0.University0.edu",
+        )
+    )
 
     # 6
     q6 = an(entity(x := let(Student, domain=None)))
 
     # 7
-    associate_professor = the(entity(
-        assoc_prof := let(AssociateProfessor, domain=None),
-        assoc_prof.uri == "http://www.Department0.University0.edu/AssociateProfessor0"
-    ))
+    associate_professor = the(
+        entity(
+            assoc_prof := let(AssociateProfessor, domain=None),
+            assoc_prof.uri
+            == "http://www.Department0.University0.edu/AssociateProfessor0",
+        )
+    )
 
     q7 = a(
         set_of(
@@ -120,40 +144,56 @@ def get_eql_queries() -> List[ResultQuantifier]:
                 z := flatten(x.takes_course),
             ),
             HasType(y, Faculty),
-            contains(
-                y.teacher_of, z
-            ),  # will benefit from symbol graph optimization
+            contains(y.teacher_of, z),  # will benefit from symbol graph optimization
         )
     )
 
     # 10
-    q10 = an(entity(
-        x := let(Student, domain=None),
-        flatten(x.takes_course).uri
-        == "http://www.Department0.University0.edu/GraduateCourse0",
-    ))
+    q10 = an(
+        entity(
+            x := let(Student, domain=None),
+            flatten(x.takes_course).uri
+            == "http://www.Department0.University0.edu/GraduateCourse0",
+        )
+    )
 
     # 11
-    q11 = an(entity(
-        x := let(ResearchGroup, domain=None),
-        flatten(x.sub_organization_of).uri == "http://www.University0.edu",
-    ))
+    q11 = an(
+        entity(
+            x := let(ResearchGroup, domain=None),
+            flatten(x.sub_organization_of).uri == "http://www.University0.edu",
+        )
+    )
 
     # 12
     q12 = an(
         set_of(
             (x := let(Chair, domain=None), y := flatten(x.works_for)),
-            exists(y, and_(HasType(y, Department)
-                   , flatten(y.sub_organization_of).uri == "http://www.University0.edu"))
+            exists(
+                y,
+                and_(
+                    HasType(y, Department),
+                    flatten(y.sub_organization_of).uri == "http://www.University0.edu",
+                ),
+            ),
             # flatten(y.sub_organization_of).uri == "http://www.University0.edu",
         )  # writing contains like this implies that the user knows that this is a set of objects.
         # A more declarative way would be to write SubOrganizationOf(y, the(University(name="University0"))).
     )
 
     # 13
-    q13 = an(entity(
-        x := flatten(the(entity(uni := let(University, domain=None), uni.uri == "http://www.University0.edu")).has_alumnus),
-    ))
+    q13 = an(
+        entity(
+            x := flatten(
+                the(
+                    entity(
+                        uni := let(University, domain=None),
+                        uni.uri == "http://www.University0.edu",
+                    )
+                ).has_alumnus
+            ),
+        )
+    )
 
     # 14
     q14 = an(entity(x := let(UndergraduateStudent, domain=None)))
