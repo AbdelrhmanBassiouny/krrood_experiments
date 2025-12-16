1a1a2a630ee3b3c16669f1299c6cc62def927f7b
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-12-17 00:34:48 +0100

    [LUBM] updating queries to fit eql updates.

--- src/krrood_experiments/lubm/lubm_eql_queries.py
+++ src/krrood_experiments/lubm/lubm_eql_queries.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from typing import List
 
 import rdflib
-from krrood.entity_query_language.entity import entity, variable, exists, flatten, and_, set_of
+from krrood.entity_query_language.entity import entity, variable, exists, flatten, and_, set_of, contains
 from krrood.entity_query_language.entity_result_processors import (
     a,
     the, an,
@@ -53,9 +53,6 @@ class QueryWithSelectables:
     A dictionary mapping variable names to selectables.
     """
 
-    def __post_init__(self):
-        self.query = a(set_of(*self.selectables.values()))
-
     def evaluate(self):
         for value in self.query.evaluate():
             if isinstance(value, UnificationDict):
@@ -66,138 +63,131 @@ class QueryWithSelectables:
 
 def get_eql_queries() -> List[QueryWithSelectables]:
     # 1 (No joining, just filtration of graduate students through taking a certain course)
-    GS = variable(GraduateStudent, domain=None)
-    TC = variable(Course, domain=GS.takes_course)
-    q1 = an(entity(GS).where(TC.uri == "http://www.Department0.University0.edu/GraduateCourse0"))
-
+    grad_student = variable(GraduateStudent, domain=None)
+    takes_course = variable(Course, domain=grad_student.takes_course)
+    q1 = an(entity(grad_student).where(takes_course.uri == "http://www.Department0.University0.edu/GraduateCourse0"))
     q1 = QueryWithSelectables(q1, {"X": q1})
 
     # 2
-    uni = variable(University, domain=None)
     gs = variable(GraduateStudent, domain=None)
-    department = variable(Department, domain=gs.person.member_of)
-    q2 = an(entity(gs).where(gs.person.undergraduate_degree_from == uni,
-                                             department.sub_organization_of == uni))
+    member_of = variable(Department, domain=gs.person.member_of)
+    under_graduate_degree_from = variable(University, domain=gs.person.undergraduate_degree_from)
+    q2 = an(entity(gs).where(contains(member_of.sub_organization_of, under_graduate_degree_from)))
 
     q2 = QueryWithSelectables(q2, {"X": q2})
 
-    # 3
-    q3 = a(
-        matching(Publication)(
-            publication_author=matching()(
-                uri="http://www.Department0.University0.edu/AssistantProfessor0",
-            )
-        )
-    )
+    #3
+    publications = variable(Publication, domain=None)
+    pub_author = variable(Person, domain=publications.publication_author)
+    q3 = an(entity(publications).where(pub_author.uri == "http://www.Department0.University0.edu/AssistantProfessor0"))
     q3 = QueryWithSelectables(q3, {"X": q3})
 
     # 4
     professor = variable(Professor, domain=None)
-    org = variable(Organization, domain=professor.works_for)
-    q4 = an(entity(professor)
-           .where(org.uri == "http://www.Department0.University0.edu")
-    )
+    works_for = variable(Organization, domain=professor.works_for)
+    q4 = a(set_of(professor,
+                  name := professor.name,
+                  email := professor.person.email_address,
+                  telephone := professor.person.telephone).where(works_for.uri == "http://www.Department0.University0.edu"))
     q4 = QueryWithSelectables(
         q4,
         {
-            "X": q4,
-            "Y1": q4.name,
-            "Y2": q4.person.email_address,
-            "Y3": q4.person.telephone,
+            "X": professor,
+            "Y1": name,
+            "Y2": email,
+            "Y3": telephone,
         },
     )
 
-    # 5
-    q5 = a(
-        matching(Person)(
-            member_of=matching()(uri="http://www.Department0.University0.edu")
-        )
-    )
-    q5 = QueryWithSelectables(q5, {"X": q5})
+    # # 5
+    # q5 = a(
+    #     matching(Person)(
+    #         member_of=matching()(uri="http://www.Department0.University0.edu")
+    #     )
+    # )
+    # q5 = QueryWithSelectables(q5, {"X": q5})
+    #
+    # # 6
+    # q6 = a(matching(Student))
+    # q6 = QueryWithSelectables(q6, {"X": q6})
+    #
+    # # 7
+    # associate_professor = the(
+    #     matching(AssociateProfessor)(
+    #         uri="http://www.Department0.University0.edu/AssociateProfessor0",
+    #     )
+    # )
+    # q7 = a(
+    #     matching(Student)(
+    #         takes_course=associate_professor.teacher_of,
+    #     )
+    # )
+    # q7 = QueryWithSelectables(q7, {"X": q7, "Y": q7.takes_course})
+    #
+    # # 8
+    # q8 = a(
+    #     matching(Student)(
+    #         person=matching()(
+    #             member_of=matching(Department)(
+    #                 sub_organization_of=matching()(uri="http://www.University0.edu")
+    #             ),
+    #         )
+    #     )
+    # )
+    # q8 = QueryWithSelectables(
+    #     q8, {"X": q8, "Y1": q8.person.member_of, "Y2": q8.person.email_address}
+    # )
+    #
+    # # 9
+    # course = matching(Course)
+    # q9 = a(
+    #     matching(Student)(
+    #         person=matching()(advisor=matching(Faculty)(teacher_of=course)),
+    #         takes_course=course,
+    #     )
+    # )
+    # q9 = QueryWithSelectables(
+    #     q9, {"X": q9, "Y1": q9.person.advisor, "Y2": q9.takes_course}
+    # )
+    #
+    # # 10
+    # q10 = a(
+    #     matching(Student)(
+    #         takes_course=matching()(
+    #             uri="http://www.Department0.University0.edu/GraduateCourse0",
+    #         )
+    #     )
+    # )
+    # q10 = QueryWithSelectables(q10, {"X": q10})
+    #
+    # # 11
+    # q11 = a(
+    #     matching(ResearchGroup)(
+    #         sub_organization_of=matching()(uri="http://www.University0.edu")
+    #     )
+    # )
+    # q11 = QueryWithSelectables(q11, {"X": q11})
+    #
+    # # 12
+    # q12 = a(
+    #     matching(Chair)(
+    #         works_for=matching(Department)(
+    #             sub_organization_of=matching()(uri="http://www.University0.edu")
+    #         )
+    #     )
+    # )
+    # q12 = QueryWithSelectables(q12, {"X": q12, "Y": q12.works_for})
+    #
+    # # 13
+    # q13 = a(matching(University)(uri="http://www.University0.edu"))
+    # q13 = QueryWithSelectables(q13, {"X": q13})
+    #
+    # # 14
+    # q14 = a(matching(UndergraduateStudent))
+    # q14 = QueryWithSelectables(q14, {"X": q14})
 
-    # 6
-    q6 = a(matching(Student))
-    q6 = QueryWithSelectables(q6, {"X": q6})
-
-    # 7
-    associate_professor = the(
-        matching(AssociateProfessor)(
-            uri="http://www.Department0.University0.edu/AssociateProfessor0",
-        )
-    )
-    q7 = a(
-        matching(Student)(
-            takes_course=associate_professor.teacher_of,
-        )
-    )
-    q7 = QueryWithSelectables(q7, {"X": q7, "Y": q7.takes_course})
-
-    # 8
-    q8 = a(
-        matching(Student)(
-            person=matching()(
-                member_of=matching(Department)(
-                    sub_organization_of=matching()(uri="http://www.University0.edu")
-                ),
-            )
-        )
-    )
-    q8 = QueryWithSelectables(
-        q8, {"X": q8, "Y1": q8.person.member_of, "Y2": q8.person.email_address}
-    )
-
-    # 9
-    course = matching(Course)
-    q9 = a(
-        matching(Student)(
-            person=matching()(advisor=matching(Faculty)(teacher_of=course)),
-            takes_course=course,
-        )
-    )
-    q9 = QueryWithSelectables(
-        q9, {"X": q9, "Y1": q9.person.advisor, "Y2": q9.takes_course}
-    )
-
-    # 10
-    q10 = a(
-        matching(Student)(
-            takes_course=matching()(
-                uri="http://www.Department0.University0.edu/GraduateCourse0",
-            )
-        )
-    )
-    q10 = QueryWithSelectables(q10, {"X": q10})
-
-    # 11
-    q11 = a(
-        matching(ResearchGroup)(
-            sub_organization_of=matching()(uri="http://www.University0.edu")
-        )
-    )
-    q11 = QueryWithSelectables(q11, {"X": q11})
-
-    # 12
-    q12 = a(
-        matching(Chair)(
-            works_for=matching(Department)(
-                sub_organization_of=matching()(uri="http://www.University0.edu")
-            )
-        )
-    )
-    q12 = QueryWithSelectables(q12, {"X": q12, "Y": q12.works_for})
-
-    # 13
-    q13 = a(matching(University)(uri="http://www.University0.edu"))
-    q13 = QueryWithSelectables(q13, {"X": q13})
-
-    # 14
-    q14 = a(matching(UndergraduateStudent))
-    q14 = QueryWithSelectables(q14, {"X": q14})
-
-    eql_queries = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14]
+    eql_queries = [q1, q2, q3, q4] #q5, q6, q7, q8, q9, q10, q11, q12, q13, q14]
     return eql_queries
-    # return [q7]
-
 
 def get_python_queries():
     """
@@ -238,7 +228,7 @@ def process_value_for_lubm_answer_comparison(value: Any):
 
 if __name__ == "__main__":
     registry = load_instances_for_lubm_with_predicates()
-    # assert Chair in registry._by_class
+    assert Chair in registry._by_class
     report_python_query_time()
     start_time = time.time()
     queries_with_selectables = get_eql_queries()
