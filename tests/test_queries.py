ea96fa9fb65f2b470330e7c897e7a598265fc2fe
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-12-17 14:12:09 +0100

    Upgraded to new krrood stuff

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,10 +1,10 @@
 import os
 
 from krrood.ormatic.dao import to_dao
-from krrood.ormatic.utils import drop_database
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker, aliased
+from krrood.ormatic.utils import drop_database, create_engine
 
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker, aliased
 from owl2bench import eql_queries, sparql_queries
 from owl2bench.orm.ormatic_interface import *
 
@@ -21,11 +21,14 @@ def test_q1(owl2_dl1):
 
     session.add(dao)
     session.commit()
-    # Create aliases for the self-join
-    p1 = aliased(PersonDAO, name="knower")
-    p2 = aliased(PersonDAO, name="known")
 
-    # Construct the query using a join condition derived from the relationship structure
-    query = select(p1, p2).join(p2, p1.persondao_knows_id == p2.database_id)
-    result = session.scalars(query).all()
-    print(len(result))
+    p1 = aliased(PersonDAO)
+    p2 = aliased(PersonDAO)
+
+    stmt = select(p1, p2).select_from(
+        persondao_knows_association.join(
+            p1, p1.database_id == persondao_knows_association.c.source_persondao_id
+        ).join(p2, p2.database_id == persondao_knows_association.c.target_persondao_id)
+    )
+
+    print(session.execute(stmt).fetchall())
