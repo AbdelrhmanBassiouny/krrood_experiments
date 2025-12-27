4df7636217f4624150cebc7f78613f6272ad8f81
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-12-27 17:45:24 +0100

    q7 fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,8 @@ def pytest_sessionstart(session) -> None:
     """
     Generate the ORM at the start of the test run.
     """
-    runpy.run_path(os.path.join("..", "scripts", "generate_orm.py"))
+
+    this_file_path = os.path.abspath(__file__)
+    runpy.run_path(
+        os.path.join(this_file_path, "..", "..", "scripts", "generate_orm.py")
+    )
