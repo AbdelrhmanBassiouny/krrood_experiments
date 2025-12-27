5ec733a7ff0fdb68f0b890604f185a76b2c5ff8b
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-12-27 17:22:29 +0100

    ORM now auto refreshes

--- scripts/generate_orm.py
+++ scripts/generate_orm.py
@@ -16,31 +16,37 @@ import owl2bench.model.publications
 import owl2bench.model.roles
 import owl2bench.model.organizations
 
-all_classes = classes_of_module(owl2bench.model.base)
-all_classes += classes_of_module(owl2bench.model.college_disciplines)
-all_classes += classes_of_module(owl2bench.model.interests)
-all_classes += classes_of_module(owl2bench.model.organizations)
-all_classes += classes_of_module(owl2bench.model.programs)
-all_classes += classes_of_module(owl2bench.model.publications)
-all_classes += classes_of_module(owl2bench.model.roles)
 
-# only keep dataclasses
-all_classes = {c for c in all_classes if is_dataclass(c)}
+def generate_orm() -> None:
+    all_classes = classes_of_module(owl2bench.model.base)
+    all_classes += classes_of_module(owl2bench.model.college_disciplines)
+    all_classes += classes_of_module(owl2bench.model.interests)
+    all_classes += classes_of_module(owl2bench.model.organizations)
+    all_classes += classes_of_module(owl2bench.model.programs)
+    all_classes += classes_of_module(owl2bench.model.publications)
+    all_classes += classes_of_module(owl2bench.model.roles)
 
-class_diagram = ClassDiagram(
-    list(sorted(all_classes, key=lambda c: c.__name__, reverse=True))
-)
+    # only keep dataclasses
+    all_classes = {c for c in all_classes if is_dataclass(c)}
 
-instance = ORMatic(
-    class_dependency_graph=class_diagram,
-    alternative_mappings=recursive_subclasses(AlternativeMapping),
-)
+    class_diagram = ClassDiagram(
+        list(sorted(all_classes, key=lambda c: c.__name__, reverse=True))
+    )
 
-instance.make_all_tables()
+    instance = ORMatic(
+        class_dependency_graph=class_diagram,
+        alternative_mappings=recursive_subclasses(AlternativeMapping),
+    )
 
-file_path = os.path.join(
-    os.path.dirname(__file__), "..", "owl2bench", "orm", "ormatic_interface.py"
-)
+    instance.make_all_tables()
 
-with open(file_path, "w") as f:
-    instance.to_sqlalchemy_file(f)
+    file_path = os.path.join(
+        os.path.dirname(__file__), "..", "owl2bench", "orm", "ormatic_interface.py"
+    )
+
+    with open(file_path, "w") as f:
+        instance.to_sqlalchemy_file(f)
+
+
+if __name__ == "__main__":
+    generate_orm()
