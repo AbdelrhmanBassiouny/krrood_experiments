6ddc807fcae627c8c4c98a1ffb4e5f5cfd4b04d3
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-12-27 22:35:08 +0100

    q13 fixed

--- owl2bench/model/base.py
+++ owl2bench/model/base.py
@@ -65,3 +65,4 @@ class Person(IdentifiedEntity):
     knows: List[Person] = field(default_factory=list)
     collaborates_with: List[Person] = field(default_factory=list)
     is_advised_by: List[Person] = field(default_factory=list)
+    takes_course: List[Course] = field(default_factory=list)
