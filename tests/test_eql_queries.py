67029879f77ecc21fc6ea380caefdeae5b0a22d4
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-01-01 21:48:53 +0100

    q21 is buggy and i fear this already comes from the ontology

--- tests/test_eql_queries.py
+++ tests/test_eql_queries.py
@@ -4,10 +4,4 @@ from owl2bench.model.college_disciplines import Engineering
 
 def test_something(world_from_graph_db):
 
-    departments = [
-        x for x in world_from_graph_db.organizations if isinstance(x, Department)
-    ]
-
-    departments_with_engineering_courses = [
-        x for x in departments if x.has_engineering_courses
-    ]
+    pass
