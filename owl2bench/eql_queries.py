d1c87ee4d2e80f04527213b7f47979fb3958aab0
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-01-02 11:49:25 +0100

    started to work on eql queries

--- owl2bench/eql_queries.py
+++ owl2bench/eql_queries.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from typing import Callable
 
-from krrood.entity_query_language.entity import variable, contains, set_of
+from krrood.entity_query_language.entity import variable, contains, set_of, entity
 from krrood.entity_query_language.entity_result_processors import an
 from krrood.entity_query_language.symbolic import (
     SymbolicExpression,
 )
 
-
+from .model.base import World, Person
 from . import sparql_queries
 
 
@@ -23,3 +23,16 @@ class EQLQuery:
     """
     A function that takes a World and returns an EQL Query.
     """
+
+
+def eql1(world: World):
+    p = variable(Person, world.persons)
+    q = an(set_of(p, p.knows))
+    return q
+
+
+q1 = EQLQuery(sparql_queries.q1, eql1)
+
+all_queries = [
+    q1,
+]
--- tests/test_eql_queries.py
+++ tests/test_eql_queries.py
@@ -1,1 +1,28 @@
-def test_something(world_from_graph_db): ...
+import pytest
+
+import owl2bench.eql_queries
+import SPARQLWrapper
+
+
+@pytest.mark.parametrize(
+    "eql_query_obj",
+    [
+        pytest.param(q, id=f"q{q.sparql_query.number}")
+        for q in owl2bench.eql_queries.all_queries
+        # if owl2bench.sparql_queries.OWLProfile.RL in q.sparql_query.profile
+    ],
+)
+def test_query(eql_query_obj, world_from_graph_db):
+
+    # Initialize connection to GraphDB
+    sparql = SPARQLWrapper.SPARQLWrapper("http://localhost:7200/repositories/KRROOD")
+    sparql.setReturnFormat(SPARQLWrapper.JSON)
+
+    # Execute query
+    sparql.setQuery(eql_query_obj.sparql_query.raw_sparql_string)
+    sparql_results = sparql.query().convert()
+    sparql_result_len = len(sparql_results["results"]["bindings"])
+
+    eql_result = list(eql_query_obj.query(world_from_graph_db).evaluate())
+    eql_result_len = len(eql_result)
+    assert sparql_result_len == eql_result_len
