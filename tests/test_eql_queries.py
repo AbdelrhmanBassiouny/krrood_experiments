cbad28864fc6defcc67a386ecc57a7d2235f38ec
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-01-02 11:04:00 +0100

    added experiments

--- tests/test_eql_queries.py
+++ tests/test_eql_queries.py
@@ -1,15 +1,1 @@
-def test_q22(world_from_graph_db):
-    """
-    Find all the students who took course taught by the Dean of the Organization.
-    """
-    results = set()
-    for organization in world_from_graph_db.organizations:
-        dean = organization.dean
-        if dean:
-            for course in world_from_graph_db.courses:
-                if dean in course.teachers:
-                    for person in world_from_graph_db.persons:
-                        if course in person.takes_course:
-                            results.add((person.identifier, course.identifier))
-
-    assert len(results) == 106
+def test_something(world_from_graph_db): ...
