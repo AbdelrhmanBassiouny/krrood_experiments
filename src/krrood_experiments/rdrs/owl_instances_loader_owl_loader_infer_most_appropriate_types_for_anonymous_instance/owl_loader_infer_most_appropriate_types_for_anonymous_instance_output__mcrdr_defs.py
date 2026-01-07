a7f4185c8e607b054fb5b0215d072ac32167f9b2
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2026-01-07 18:15:11 +0100

    [OwlToPython] need to infer types from ranges of descriptors on anonymous instances.

--- src/krrood_experiments/rdrs/owl_instances_loader_owl_loader_infer_most_appropriate_types_for_anonymous_instance/owl_loader_infer_most_appropriate_types_for_anonymous_instance_output__mcrdr_defs.py
+++ src/krrood_experiments/rdrs/owl_instances_loader_owl_loader_infer_most_appropriate_types_for_anonymous_instance/owl_loader_infer_most_appropriate_types_for_anonymous_instance_output__mcrdr_defs.py
@@ -1,15 +1,13 @@
-from krrood.class_diagrams.utils import (
-    Role,
-    sort_classes_by_role_aware_inheritance_path_length,
-)
-from ripple_down_rules.datastructures.case import Case
-from typing_extensions import Dict, List, Set, Union
-from krrood_experiments.owl_instances_loader import OwlLoader
 from krrood_experiments.utils import (
+    AnonymousClass,
     get_most_specific_types,
     get_non_class_attribute_names_of_instance,
-    AnonymousClass,
 )
+from typing_extensions import Dict, List, Set, Union
+from krrood.class_diagrams.utils import Role, nearest_common_ancestor
+from krrood.entity_query_language.entity import has_solution
+from krrood_experiments.owl_instances_loader import OwlLoader
+from abc import ABC
 from ripple_down_rules import *
 
 
@@ -71,19 +69,12 @@ def conclusion_178686770575922948619403589971234626498(case) -> List[type]:
     return owl_loader_infer_most_appropriate_types_for_anonymous_instance(**case)
 
 
-def conditions_107546415252908647157514226735385206217(case) -> bool:
+def conditions_39422379577793614665216170177046282573(case) -> bool:
     def conditions_for_owl_loader_infer_most_appropriate_types_for_anonymous_instance(
         self_: OwlLoader, instance: AnonymousClass, **kwargs
     ) -> bool:
         """Get conditions on whether it's possible to conclude a value for owl_loader_infer_most_appropriate_types_for_anonymous_instance.output_  of type ."""
-        return (
-            len(
-                case.self_.get_inferred_types_from_descriptors_domains_of_instance(
-                    case.instance
-                )
-            )
-            > 1
-        )
+        return not case.output_
 
     return (
         conditions_for_owl_loader_infer_most_appropriate_types_for_anonymous_instance(
@@ -92,19 +83,31 @@ def conditions_107546415252908647157514226735385206217(case) -> bool:
     )
 
 
-def conclusion_107546415252908647157514226735385206217(case) -> List[type]:
+def conclusion_39422379577793614665216170177046282573(case) -> List[type]:
     def owl_loader_infer_most_appropriate_types_for_anonymous_instance(
         self_: OwlLoader, instance: AnonymousClass, **kwargs
     ) -> List[type]:
         """Get possible value(s) for owl_loader_infer_most_appropriate_types_for_anonymous_instance.output_  of type ."""
-        possible_types = self_.get_inferred_types_from_descriptors_domains_of_instance(
+        contesting_types = set()
+        inferred_types = self_.get_inferred_types_from_descriptors_domains_of_instance(
             instance
         )
-
-        possible_types.update(instance.types)
-
-        possible_types = get_most_specific_types(possible_types)
-
-        return sort_classes_by_role_aware_inheritance_path_length(possible_types)
+        explicit_types = instance.types
+        all_types = list(set(inferred_types).union(set(explicit_types)))
+        for i, c1 in enumerate(all_types[:-1]):
+            for c2 in all_types[i + 1 :]:
+                nca = nearest_common_ancestor([c1, c2])
+                if nca in [Role, ABC, object, None]:
+                    continue
+                contesting_types.update({c1, c2})
+        non_contesting_types = set(all_types) - contesting_types
+        types_with_satisfied_axioms = [
+            t
+            for t in contesting_types
+            if not hasattr(t, "axiom") or has_solution(instance, t.axiom)
+        ]
+        final_types = non_contesting_types.union(set(types_with_satisfied_axioms))
+        most_specific_types = get_most_specific_types(final_types)
+        return most_specific_types
 
     return owl_loader_infer_most_appropriate_types_for_anonymous_instance(**case)
