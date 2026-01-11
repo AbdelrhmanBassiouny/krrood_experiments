a0368f8d4d712fdcb6fe9df137ba186701e75c60
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2026-01-11 01:26:57 +0100

    [OWL2Bench] until q10 working.

--- src/krrood_experiments/owl2bench/owl2bench_eql_queries.py
+++ src/krrood_experiments/owl2bench/owl2bench_eql_queries.py
@@ -66,8 +66,8 @@ def get_eql_queries(
     q3 = QueryWithSelectables(q3, {"x": o1, "y": o2}, 3)
 
     p = variable(Person, domain=None)
-    q4 = an(entity(p.has_age).where(p.has_age))
-    q4 = QueryWithSelectables(q4, {"x": p}, 4)
+    q4 = an(set_of(p, p.has_age).where(p.has_age))
+    q4 = QueryWithSelectables(q4, {"x": p, "y": p.has_age}, 4)
 
     p = variable(T20CricketFan, None)
     q5 = an(entity(p))
@@ -83,41 +83,43 @@ def get_eql_queries(
     q8 = an(set_of(o1, o2))
     q8 = QueryWithSelectables(q8, {"x": o1, "y": o2}, 8)
 
-    o1 = variable(College, domain=None)
-    c = variable_from(o1.has_college_discipline)
-    q9 = an(set_of(o1, c).where(not_(HasType(c, Science))))
-    q9 = QueryWithSelectables(q9, {"x": o1, "y": c}, 9)
+    # o1 = variable(College, domain=None)
+    # c = variable_from(o1.has_college_discipline)
+    # q9 = an(set_of(o1, c).where(not_(HasType(c, Science))))
+    # q9 = QueryWithSelectables(q9, {"x": o1, "y": c}, 9)
 
-    # p1 = variable(Person, domain=None)
-    # p2 = variable_from(p1.has_collaboration_with)
-    # q10 = an(set_of(p1, p2))
-    #
-    # p1 = variable(Person, domain=None)
-    # p2 = variable_from(p1.is_advised_by)
-    # q11 = an(set_of(p1, p2))
-    #
-    # p1 = variable(Person, domain=None)
-    # q12 = an(entity(p1))
-    #
-    # wc = variable(WomanCollege, domain=None)
-    # q13 = an(entity(wc))
+    p1 = variable(Person, domain=None)
+    p2 = variable_from(p1.has_collaboration_with)
+    q10 = an(set_of(p1, p2))
+    q10 = QueryWithSelectables(q10, {"x": p1, "y": p2}, 10)
 
-    # ls = variable(LeisureStudent, domain=None)
-    # q14 = an(entity(ls))
-    #
+    p1 = variable(Person, domain=None)
+    p2 = variable_from(p1.is_advised_by)
+    q11 = an(set_of(p1, p2))
+    q11 = QueryWithSelectables(q11, {"x": p1, "y": p2}, 11)
+
+    p1 = variable(Person, domain=None)
+    q12 = an(entity(p1))
+    q12 = QueryWithSelectables(q12, {"x": p1}, 12)
+
     # o = variable(Organization, domain=None)
     # heads = variable_from(o.has_head)
     # q15 = an(entity(heads))
-    #
+
     # o = variable(Organization, domain=None)
     # head = variable_from(o.has_h)
     # q16 = an(entity(o).where(exists(o, o.has_head)))
-    #
+
     # ugs = variable(UGStudent, domain=None)
     # q17 = an(entity(ugs))
     # q17 = QueryWithSelectables(q17, {"X": ugs}, 17)
 
-    eql_queries = [q2, q3, q4, q5, q7, q8, q9]  # , q10, q11, q12, q13]
+    p1 = variable(Person, domain=None)
+    p2 = variable_from(p1.has_same_home_town_with)
+    q20 = an(set_of(p1, p2))
+    q20 = QueryWithSelectables(q20, {"x": p1, "y": p2}, 20)
+
+    eql_queries = [q2, q3, q4, q5, q7, q8, q10, q11, q12, q20]
     return eql_queries
 
 
@@ -147,8 +149,8 @@ if __name__ == "__main__":
     queries_with_selectables = get_eql_queries(instances_for_class)
     counts, results, times = evaluate_eql(queries_with_selectables)
     end_time = time.time()
-    for i, count_ in enumerate(counts, 1):
-        print(f"{i}:{count_} ({times[i - 1]} sec)")
+    for i, (r, count_) in enumerate(zip(results, counts)):
+        print(f"{r}:{count_} ({times[i]} sec)")
         # print([r for r in results[i - 1]])
     print(f"Time elapsed: {end_time - start_time} seconds")
 
@@ -172,6 +174,8 @@ if __name__ == "__main__":
             flat_r = {k: v["value"] for k, v in r.items()}
             sparql_answers[q.number].append(set(tuple(flat_r.items())))
     for i, query_results in results.items():
+        if i not in sparql_answers:
+            continue
         uri_results = []
         for res in query_results:
             for k, v in res.items():
@@ -191,6 +195,12 @@ if __name__ == "__main__":
                 import pdbpp
 
                 pdbpp.set_trace()
-        assert len(sparql_answers[i]) == len(
-            uri_results
-        ), f"Number of results mismatch for query {i}"
+        try:
+            assert len(sparql_answers[i]) == len(
+                uri_results
+            ), f"Number of results mismatch for query {i}"
+        except AssertionError as e:
+            print(f"Number of results mismatch for query {i}")
+            import pdbpp
+
+            pdbpp.set_trace()
