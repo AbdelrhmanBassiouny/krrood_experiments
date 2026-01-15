f6361c57b77bd38147bd245c1df95c3861610663
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2026-01-15 12:59:07 +0100

    [OWL2Bench] debugging reloading.

--- src/krrood_experiments/owl_instances_loader.py
+++ src/krrood_experiments/owl_instances_loader.py
@@ -475,7 +475,14 @@ class OwlLoader:
             for s, ai in self.anonymous_instances.items()
             for o_class in ai.final_sorted_types
         )
+        seen_s = set()
         for s, py_cls in so_iterator:
+            if s not in seen_s:
+                seen_s.add(s)
+            else:
+                import pdbpp
+
+                pdbpp.set_trace()
             existing_roles = self.registry.resolve(s)
             kwargs = self._get_common_role_taker_kwargs(existing_roles, py_cls)
             self.registry.get_or_create_for(s, py_cls, self.symbol_graph, **kwargs)
