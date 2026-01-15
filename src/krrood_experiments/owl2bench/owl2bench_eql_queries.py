d475f3c040b77aa43c782be5e60901a89f46b8be
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2026-01-15 01:15:23 +0100

    [OWL2Bench] before junie cleaning.

--- src/krrood_experiments/owl2bench/owl2bench_eql_queries.py
+++ src/krrood_experiments/owl2bench/owl2bench_eql_queries.py
@@ -192,7 +192,7 @@ def process_value_for_owl2bench_answer_comparison(value: Any):
 
 if __name__ == "__main__":
     loading_start_time = time.time()
-    registry = load_instances_for_owl2bench_with_predicates(reasoned=False)
+    registry = load_instances_for_owl2bench_with_predicates(reasoned=True, clean=False)
     loading_time = time.time() - loading_start_time
     print(f"Loading time: {loading_time} seconds")
 
