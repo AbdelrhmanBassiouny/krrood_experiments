ae86c76d61f91f2de7717db3d1b4b84205dff1e8
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2026-01-23 18:30:24 +0100

    [OWL2Bench] any and all

--- scripts/eql_random_events_test.py
+++ scripts/eql_random_events_test.py
@@ -10,7 +10,9 @@ from krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates_base impor
 import krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates
 from random_events.variable import Symbolic, Set, Integer
 
-classes = classes_of_module(krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates) + [OWL2BenchThing, Symbol]
+classes = classes_of_module(
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates
+) + [OWL2BenchThing, Symbol]
 
 class_diagram = ClassDiagram(classes)
 symbol_graph = SymbolGraph(class_diagram)
@@ -21,7 +23,11 @@ symbol_graph = SymbolGraph(class_diagram)
 inheritance_graph = class_diagram.inheritance_subgraph_without_unreachable_nodes
 symbol_graph.to_dot(graph=inheritance_graph, filepath="inheritance_graph.svg")
 
-leaves = [node.clazz for node in inheritance_graph.nodes() if inheritance_graph.out_degree(node.index) == 0]
+leaves = [
+    node.clazz
+    for node in inheritance_graph.nodes()
+    if inheritance_graph.out_degree(node.index) == 0
+]
 
 class_variable = Symbolic("type", Set.from_iterable(leaves))
 
@@ -32,23 +38,48 @@ for node in inheritance_graph.nodes():
         if issubclass(leaf, node.clazz):
             current_possible_sets.append(leaf)
     if len(current_possible_sets) > 0:
-        possible_sets[node.clazz] = (class_variable.make_value(current_possible_sets))
+        possible_sets[node.clazz] = class_variable.make_value(current_possible_sets)
 # print(*[f"{k.__name__} = {v}" for k, v in possible_sets.items()], sep="\n")
 
 
-college_discipline = possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.CollegeDiscipline]
-student = possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Student]
-non_science = ~possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Science] & college_discipline
-engineering = possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Engineering]
-science = possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Science]
-leisure_student = possible_sets[krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.LeisureStudent]
-print(krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.LeisureStudent.axiom( None))
+college_discipline = possible_sets[
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.CollegeDiscipline
+]
+student = possible_sets[
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Student
+]
+non_science = (
+    ~possible_sets[
+        krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Science
+    ]
+    & college_discipline
+)
+engineering = possible_sets[
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Engineering
+]
+science = possible_sets[
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Science
+]
+leisure_student = possible_sets[
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.LeisureStudent
+]
+print(
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.LeisureStudent.axiom(
+        None
+    )
+)
 
 # IsSubClassOrRole(variable_from(candidate_var.types), Student) >= 1
 #
 exists = Integer("exists")
-types_with_student = [krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Lecturer, krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.UGStudent]
-types_without_student = [krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Lecturer, krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Professor]
+types_with_student = [
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Lecturer,
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.UGStudent,
+]
+types_without_student = [
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Lecturer,
+    krrood_experiments.owl2bench.ontomatic.owl2bench_with_predicates.Professor,
+]
 
 candidate_var_types = possible_sets[types_without_student[0]]
 for type_ in types_without_student[1:]:
@@ -57,5 +88,21 @@ candidate_var_types &= student
 
 print(not candidate_var_types.is_empty())
 
+print(
+    any(
+        not (possible_sets[type_] & student).is_empty()
+        for type_ in types_without_student
+    )
+)
+
+print(
+    all(
+        not (possible_sets[type_] & student).is_empty()
+        for type_ in types_without_student
+    )
+)
+
 course_length = Integer("course_length")
-course_length_restriction = SimpleInterval(lower=-np.inf, upper=1, left=Bound.OPEN, right=Bound.CLOSED)
+course_length_restriction = SimpleInterval(
+    lower=-np.inf, upper=1, left=Bound.OPEN, right=Bound.CLOSED
+)
